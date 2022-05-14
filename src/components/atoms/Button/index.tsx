2c17ca601bc78014506e44157261c7084e042bbc
SteveJamerson <stevejamerson@outlook.com>
2022-05-14 14:57:30 -0300

    feat: created button

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,46 @@
+import clsx from "clsx";
+import React from "react";
+import { Icon } from "../Icon";
+import { IconName } from "../Icon/Icon.types";
+import { ButtonProps } from "./Button.interfaces";
+
+export const Button: React.FC<ButtonProps> = ({
+   className,
+   variant = "primary",
+   text,
+   type = "button",
+   loading = false,
+   disabled,
+   children,
+   iconName,
+   iconPosition = iconName ? "end" : "",
+   iconSize,
+   ...props
+}) => {
+   const classes = clsx([
+      {
+         button: true,
+         [`button--${variant}`]: variant,
+         [`button--loading`]: loading,
+      },
+      className,
+   ]);
+
+   return (
+      <button
+         className={classes}
+         type={type}
+         disabled={loading || disabled}
+         {...props}
+      >
+         {iconPosition === "start" && (
+            <Icon name={iconName as IconName} size={iconSize} />
+         )}
+         {loading ? <Icon name="loader-line" /> : text || children}
+
+         {iconPosition === "end" && (
+            <Icon name={iconName as IconName} size={iconSize} />
+         )}
+      </button>
+   );
+};
